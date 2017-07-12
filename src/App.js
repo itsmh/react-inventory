c19b456635478616f3cdfbb59e0a626c8739d5d4
Masood Hamzehkhany <peace.mhy@gmail.com>
2017-07-12 16:22:05 +0430

    Added font-awesome and finished html page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-//import $ from 'jquery';
-//import Bootstrap from 'bootstrap';
 import axios from 'axios';
 import Item from './Item'
 import 'bootstrap/dist/css/bootstrap.css';
@@ -14,25 +12,47 @@ class App extends Component {
     this.state = {
       posts: []
     };
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   componentDidMount() {
+    axios.defaults.headers.post['Content-Type'] = 'application/json';
     axios.get(`http://localhost/data.json`)
       .then(res => {
-        //const posts = res.rgDescriptions.children.map(obj => obj.rgDescriptions);
         const posts = Object.keys(res.data.rgDescriptions).map(obj => res.data.rgDescriptions[obj]);
-        console.log(res.data.rgDescriptions);
-        // console.log($().jquery);
         this.setState({ posts });
       });
   }
 
-  
+  handleSubmit(e) {
+    console.log(e);
+    e.preventDefault();
+    let result = {};
+    let data = new FormData(document.querySelector('form'));
+    for (var entry of data.entries())
+    {
+        if(/.*\[\]/i.test(entry[0])) {
+          result[entry[0]] = result[entry[0]] || [];
+          result[entry[0]].push(entry[1]);
+        }else {
+          result[entry[0]] = entry[1];
+        }
+    }
+    result = JSON.stringify(result)
+
+    axios.post(`http://172.16.39.57:8088/`,result);
+  }
+
+
   render() {
     return (
-      <form onSubmit="">
+      <form onSubmit={this.handleSubmit} >
         <div className="row">
-          {this.state.posts.map((post) => <Item data={post} / >)}
+          {this.state.posts.map((post) => 
+            <Item 
+              key={post.classid + '_' + post.instanceid} 
+              data={post} / >)}
         </div>
+        <input className="btn btn-success" type="submit" />
       </form>
     );
   }
